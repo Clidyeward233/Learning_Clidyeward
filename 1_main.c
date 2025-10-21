1b23b4524f9f2f3be9e7d3bcd45f7d52291048b8
Clidyeward <54670317+Clidyeward233@users.noreply.github.com>
2025-10-21 18:10:11 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,9 +1,12 @@
-//
-//
-//
+//202510414210
+//493758308@qq.com
+//周博渊
 #include <stdio.h>
-int main()
-{
-    printf("Hello World");
+int main(){
+    float a,b;
+    scanf("%f",&a);
+    b =a*(9.0/5) + 32;
+    printf("%.1f",b);
     return 0;
+     
 }
