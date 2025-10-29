d978aa677eeb6fa0f5c8fe56f077fc178739eb9e
Clidyeward <54670317+Clidyeward233@users.noreply.github.com>
2025-10-29 11:21:23 +0800

    Update 4_main.c

--- 4_main.c
+++ 4_main.c
@@ -2,9 +2,22 @@
 //493758308@qq.com
 //周博渊
 #include <stdio.h>
-
+#include <math.h>
 int main(){
-    int a,b,c;
-    scanf("%d %d %d",&a,&b,&c);
-    (a+b>c && b+c>a && c+a>b)?printf("可以组成三角形"):printf("不能组成三角形");
+    int i=1;
+    int k,j=0;
+    while(i<=9){
+        j=0;
+        while(j<=9){
+            k=0;
+            while(k<=9){
+                if(100*i+10*j+k==pow(i,3)+pow(j,3)+pow(k,3)){
+                    printf("%d ",(int)100*i+10*j+k);
+                }
+                k++;
+            }
+            j++;
+        }
+        i++;
+    }
 }
