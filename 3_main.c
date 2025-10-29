c57c23a6f64861b3e757038a78ae12fa4e58089d
Clidyeward <54670317+Clidyeward233@users.noreply.github.com>
2025-10-29 11:21:11 +0800

    Update 3_main.c

--- 3_main.c
+++ 3_main.c
@@ -2,24 +2,22 @@
 //493758308@qq.com
 //周博渊
 #include <stdio.h>
-
 int main(){
-    int a,b;
-    char q;
-    scanf("%d %d %c",&a,&b,&q);
-    if (q=='+'){
-        printf("%d",a+b);
-    }else if(q=='-'){
-        printf("%d",a-b);
+    int a=0;
+    int i=1;
+    int k=0;
+    scanf("%d",&a);
+    while(i<50){
+        if (a%i==0){
+            k++;
+        }else{
+            break;
+        }
+        i++;
     }
-    else if(q=='*'){
-        printf("%d",a*b);
-    }
-    else if(q=='/'){
-        printf("%d",a/b);
-    }
-    else{
-        printf("Worring");
-    }
-    return 0;
+    if (k==2){
+        printf("密钥不安全，请重新输入");
+    }else{
+        printf("密钥安全，密码设置成功");
+    } 
 }
