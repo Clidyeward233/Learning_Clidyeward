847a7ae513429f0260da4e4dc3271c376652784a
Clidyeward <54670317+Clidyeward233@users.noreply.github.com>
2025-10-29 11:20:57 +0800

    Update 2_main.c

--- 2_main.c
+++ 2_main.c
@@ -2,21 +2,16 @@
 //493758308@qq.com
 //周博渊
 #include <stdio.h>
-
-int main ()
-{
-    int a;
-    scanf("%d",&a);
-    if(a>=90){
-        printf("A");
-    }else if(a>=80 && a<=89){
-        printf("B");
-    }else if(a>=70 && a<=79){
-        printf("C");
-    }else if(a>=60 && a<=69){
-        printf("D");
-    }else if(a<60){
-        printf("E");
+#include <math.h>
+int main(){
+    int i,j,k;
+    for(i=1;i<=9;i++){
+        for(j=0;j<=9;j++){
+            for(k=0;k<=9;k++){
+                if(100*i+10*j+k==pow(i,3)+pow(j,3)+pow(k,3)){
+                    printf("%d ",(int)100*i+10*j+k);
+                }
+            }
+        }
     }
-    return 0;
 }
