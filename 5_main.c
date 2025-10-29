8af560142bd158b90cfe9258ae50c6d0bd8c5dde
Clidyeward <54670317+Clidyeward233@users.noreply.github.com>
2025-10-29 11:21:57 +0800

    Create 5_main.c

---
+++ 5_main.c
@@ -0,0 +1,11 @@
+//202510414210
+//493758308@qq.com
+//周博渊
+#include <stdio.h>
+int main(){
+    int i,a,b,c,d;
+    scanf("%d%d%d%d",&a,&b,&c,&d);
+    i=a+b+c+d;
+    printf("%d %d %d %d %d",a,b,c,d,i);
+
+}
