2234f4446b19137c6ac96c65b79cf0597e90c882
Clidyeward <54670317+Clidyeward233@users.noreply.github.com>
2025-11-07 20:40:51 +0800

    Update 2_main.c

--- 2_main.c
+++ 2_main.c
@@ -2,16 +2,23 @@
 //493758308@qq.com
 //周博渊
 #include <stdio.h>
-#include <math.h>
-int main(){
-    int i,j,k;
-    for(i=1;i<=9;i++){
-        for(j=0;j<=9;j++){
-            for(k=0;k<=9;k++){
-                if(100*i+10*j+k==pow(i,3)+pow(j,3)+pow(k,3)){
-                    printf("%d ",(int)100*i+10*j+k);
-                }
-            }
+
+int main() {
+    int matrix[3][3];
+    int i, j;
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            scanf("%d", &matrix[i][j]);
         }
     }
+    for (j = 0; j < 3; j++) {
+        for (i = 0; i < 3; i++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
+
+
