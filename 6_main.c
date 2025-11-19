5759ba1787d8731293d66dd779b972c6ac54782a
Clidyeward <54670317+Clidyeward233@users.noreply.github.com>
2025-11-19 11:48:15 +0800

    Update 6_main.c

--- 6_main.c
+++ 6_main.c
@@ -2,30 +2,16 @@
 //493758308@qq.com
 //周博渊
 #include <stdio.h>
+#include <stdlib.h>
 
-int sum(int arr[], int length) {
-    int total = 0;
-    for (int i = 0; i < length; i++) {
-        total += arr[i];
-    }
-    return total;
-}
+int main(){
+    int *arr = (int*) malloc(5*sizeof(int));
 
-int product(int arr[], int length) {
-    int total = 1;
-    for (int i = 0; i < length; i++) {
-        total *= arr[i];
-    }
-    return total;
-}
-
-int main() {
-    int arr[5];
-    for (int i=0;i<5;i++){
+    for (int i = 0 ; i<5 ; i++){
         scanf("%d",&arr[i]);
     }
-    int length = sizeof(arr) / sizeof(arr[0]);
-    
-    printf("Sum: %d\n", sum(arr, length));
-    printf("Product: %d\n", product(arr, length));
+    for (int i = 0 ;i<5 ; i++){
+        printf("%d ",arr[i]);
+    }
+    free(arr);
 }
