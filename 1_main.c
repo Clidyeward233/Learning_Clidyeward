295a10e3d2441028fb652f9da53f3d8ed24cd482
Clidyeward <54670317+Clidyeward233@users.noreply.github.com>
2025-11-19 11:46:58 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -3,22 +3,10 @@
 //周博渊
 #include <stdio.h>
 
-int main() {
-    int matrix[3][3];
-    int i, j;
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
-            scanf("%d", &matrix[i][j]);
-        }
-    }
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
-            printf("%d ", matrix[i][j]);
-        }
-        printf("\n");
-    }
-
-    return 0;
+int main(){
+    int a = 0;
+    scanf("%d",&a);
+    int *p = &a;
+    *p = *p + 10;
+    printf("%d,%d",a,*p);
 }
-
-
