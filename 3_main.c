511122023ad7137226d7ab819b0be26dbe03e2fb
Clidyeward <54670317+Clidyeward233@users.noreply.github.com>
2025-11-19 11:47:30 +0800

    Update 3_main.c

--- 3_main.c
+++ 3_main.c
@@ -2,25 +2,17 @@
 //493758308@qq.com
 //周博渊
 #include <stdio.h>
-int main(){
-    int list[10];
-    for(int i=0;i<10;i++){
-        int input = 0;
-        scanf("%d",&input);
-        list[i]=input;
-    }
-    for(int k=0;k<10;k++){
-        for(int i = 0;i<10;i++){
-            if(list[i] > list[i+1]){
-            int middle = list[i];
-            list[i]=list[i+1];
-            list[i+1]=middle;
-            }
-        
 
-        }
-    }
-    for(int i =0;i<10;i++){
-        printf("%d ",list[i]);
-    }
+void swap(int *a,int*b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+
+int main(){
+    int a,b;
+    scanf("%d %d",&a,&b);
+    swap(&a,&b);
+    printf("%d %d",a,b);
 }
